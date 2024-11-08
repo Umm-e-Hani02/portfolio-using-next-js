c79130db7743b9b15156633bfdc0ff3331d17b1e
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-08 17:22:59 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,9 +7,9 @@ export default function About(){
             <div id="content">
                 <h2>About Me</h2> <br />
                 <p>
-                    Hello! I am Umm-E-Hani a passionate 14-year-old aspiring full-stack <br /> developer with a keep interest in AI Metaverse and Web 3.0. Currently <br /> I am a student under the Governor Sindh IT Initiative (GIAIC) where I am <br /> exploring technologies like TypeScript Next.js Tailwind CSS HTML and <br /> CSS. I love creating interactive web applications CLI tools and responsive <br /> websites  turning my ideas into engaging user experiences. <br /> <br />
+                    Hello! I am Umm-E-Hani a passionate 14-year-old aspiring full-stack <br /> developer with a keep interest in AI Metaverse and Web 3.0. Currently <br /> I am a student under the Governor Sindh IT Initiative (GIAIC) where I am <br /> exploring technologies like TypeScript Next.js Tailwind CSS HTML and <br /> CSS. I love creating interactive web applications CLI tools and responsive <br /> websites  turning my ideas into engaging user experiences.<br /> <br />
 
-                    Beyond coding I enjoy sketching adding a creative edge to my projects. <br /> My journey in tech has just begun and I am excited to keep building learning <br /> and growing in this ever-evolving field. I aim to be certified in cloud applied <br />generative AI and contribute meaningfully to the world of technology. <br /> <br />
+                    Beyond coding I enjoy sketching adding a creative edge to my projects. <br /> My journey in tech has just begun and I am excited to keep building learning <br /> and growing in this ever-evolving field. I aim to be certified in cloud applied <br />generative AI and contribute meaningfully to the world of technology.<br /> <br />
 
                     Lets connect and build something great together!
                 </p>
