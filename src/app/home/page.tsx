18e9fa4f64c3652f1186f7f2ee250154926eb8d6
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-11-08 17:03:52 +0500

    nextjs milestone2 portfolio

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,11 +7,11 @@ export default function HomeSection() {
           I am <span>UMM-E-HANI</span>
         </h1>
         <p>
-          I'm on a journey to bring creativity and technology together to create
+          I' am on a journey to bring creativity and technology together to create
           engaging <br /> web experiences. As I explore the world of web
-          development, AI, and the Metaverse, <br /> I'm constantly learning,
-          building, and evolving. Here you can check out my <br />
-          ongoing projects and the skills I'm developing along the way.
+          development AI and the Metaverse <br /> I am constantly learning
+          building and evolving. Here you can check out my <br />
+          ongoing projects and the skills I am developing along the way.
         </p>
 
         <div>
